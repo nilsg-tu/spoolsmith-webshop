cda6d01f155836e548aae5984c34b671b9d944aa
Nils Gutensohn <nils.gutensohn@students.tuwien.com>
2026-05-17 22:55:47 +0200

    various fixes in detailed descriptions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const buttons = document.querySelectorAll(".price-card button");
+const pricingButtons = document.querySelectorAll(".price-card button");
 
-buttons.forEach((button) => {
+pricingButtons.forEach((button) => {
   button.addEventListener("click", () => {
     alert("Dies ist ein gemockter Kaufprozess für das Praxisbeispiel.");
   });
